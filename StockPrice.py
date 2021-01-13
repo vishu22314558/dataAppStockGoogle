ea5255b12a50b95def79166931296ab2e363c816
vishu22314558 <wishcloudsingh@gmail.com>
2021-01-13 00:10:40 -0600

    v1

---
+++ StockPrice.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+
+#print("Testing")
+
+st.write("""
+# Simple Stock Price App
+Shown are the stock **closing price** and ***volume*** of Google!
+""")
+
+tickerSymbol = 'GOOGL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.dataframe(tickerDf)
+
+st.write("""
+# Closing Price
+""")
+st.line_chart(tickerDf.Close)
+st.write("""
+# Volume Price
+""")
+st.line_chart(tickerDf.Volume)
+
+
